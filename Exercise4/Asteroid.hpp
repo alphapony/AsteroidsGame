a05bc3f2b0b0e1957077f5a3f9eb34a7281564b4
vesk <vesk@itu.dk>
2022-09-26 17:27:56 +0200

    Formatting

--- Exercise4/Asteroid.hpp
+++ Exercise4/Asteroid.hpp
@@ -5,9 +5,13 @@
 class Asteroid : public GameObject, public Collidable {
 public:
     Asteroid(const sre::Sprite& sprite);
+    
     float getRadius() const;
+
     void onCollision(std::shared_ptr<GameObject> other) override;
+    
     void setRadius(float radius);
+    
     glm::vec2 velocity;
 
 private:
